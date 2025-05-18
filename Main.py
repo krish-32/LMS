9932237cb67d740edf0105b24685ff823ea8c417
SRIKRISHNARAJAN N <132248518+krish-32@users.noreply.github.com>
2025-05-19 02:07:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -498,4 +498,5 @@ def chat():
 if __name__ == '__main__':
     # if not os.path.exists(DATABASE):
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
